0ba5b7de3d944c70365111ef69e657507541e089
vastlaan <vastlaan@mail.com>
2021-01-12 20:20:49 +0100

    Imporve responsivnes of all pages ;

--- src/js/images/portfolio.js
+++ src/js/images/portfolio.js
@@ -1,5 +1,6 @@
 // images
 import Logo from "../../img/logo.svg";
+import Deco1 from "../../img/decoration-6.svg";
 import Noir from "../../img/project-noir.jpg";
 
 // loading images
@@ -12,3 +13,6 @@ logo2.src = Logo;
 
 const noir = document.querySelector("#portfolio-image-1");
 noir.src = Noir;
+
+const deco11 = document.querySelector("#decoration-11");
+deco11.src = Deco1;
