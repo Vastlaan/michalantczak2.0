a2fee495d71dfe47c421cea2b0396223b5316b12
vastlaan <vastlaan@mail.com>
2021-01-26 14:06:04 +0100

    add silentlyFetchUnpaidHerokuApps function

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,13 @@ import "./images/landing";
 import "./slider";
 import "./events";
 import "./footer";
+
+//call the heroku-unpaid project's pages, so they already preload
+
+(function slientlyFetchUnpaidHerokuApps() {
+    fetch("https://clairehempshop.herokuapp.com", {
+        mode: "no-cors",
+    })
+        .then((res) => null)
+        .catch((e) => console.error(e));
+})();
